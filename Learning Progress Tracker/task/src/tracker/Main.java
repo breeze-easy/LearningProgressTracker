028fec1ac34ec937642a74822a5bb3265198e054
breeze-easy <androdev.cft6@gmail.com>
2022-07-06 04:54:51 -0400

    comment in Main

--- Learning Progress Tracker/task/src/tracker/Main.java
+++ Learning Progress Tracker/task/src/tracker/Main.java
@@ -2,6 +2,7 @@ package tracker;
 
 public class Main {
     public static void main(String[] args) {
+        // consider injecting Session in UI
         Session session = new Session(mode.TOPMENU, new Storage());
         UI.printProgramTitle();
         session.processUserInput();
